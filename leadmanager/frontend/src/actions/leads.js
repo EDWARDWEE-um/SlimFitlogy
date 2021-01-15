35b4bf7926cc5a49ddba0ca3a9ad526c2e2f6f8f
EDWARDWEE-um <edwardwee11@gmail.com>
2021-01-16 01:13:09 +0800

    Part 4

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { createMessage } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
 // Get LEADS
 export const getLeads = () => dispatch => {
@@ -15,8 +16,9 @@ export const getLeads = () => dispatch => {
 
 // Delete LEADS
 export const deleteLead = (id) => dispatch => {
-    axios.get(`/api/leads/${id}/`)
+    axios.delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({ deleteLead: 'Lead Deleted '}))
              dispatch ({
                 type: DELETE_LEAD,
                 payload: id
@@ -28,9 +30,19 @@ export const deleteLead = (id) => dispatch => {
 export const addLead = (lead) => dispatch => {
     axios.post("/api/leads/",lead)
         .then(res => {
+            dispatch(createMessage({ AddLead: 'Lead Added '}))
              dispatch ({
-                type: ADD_LEADS,
+                type: ADD_LEAD,
                 payload: res.data
         });
-    }).catch(err =>console.log(err));
+    }).catch(err =>{
+        const errors = {
+            msg: err.response.data,
+            status: err.response.status
+        }
+        dispatch ({
+            type: GET_ERRORS,
+            payload: errors
+        });
+    });
 }
