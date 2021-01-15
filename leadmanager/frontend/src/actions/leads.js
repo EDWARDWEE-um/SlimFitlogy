cf86cb458c34c1ab2a3113a8ec6a3e9ccbab0ed4
EDWARDWEE-um <edwardwee11@gmail.com>
2021-01-15 22:01:13 +0800

    part 3

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+
+// Get LEADS
+export const getLeads = () => dispatch => {
+    axios.get("/api/leads/")
+        .then(res => {
+             dispatch ({
+                type: GET_LEADS,
+                payload: res.data
+        });
+    }).catch(err =>console.log(err));
+}
+
+// Delete LEADS
+export const deleteLead = (id) => dispatch => {
+    axios.get(`/api/leads/${id}/`)
+        .then(res => {
+             dispatch ({
+                type: DELETE_LEAD,
+                payload: id
+        });
+    }).catch(err =>console.log(err));
+}
+
+// Add LEADS
+export const addLead = (lead) => dispatch => {
+    axios.post("/api/leads/",lead)
+        .then(res => {
+             dispatch ({
+                type: ADD_LEADS,
+                payload: res.data
+        });
+    }).catch(err =>console.log(err));
+}
